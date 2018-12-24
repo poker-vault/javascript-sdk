56bf06600192be1efc76524a71ad8fb363e46aa8
Feek McDermott <fiachra@squareup.com>
2018-12-23 20:41:43 -0800

    change: instead of returning an instance of a store, expose a function for creating the store so that we can pass in the proper vue dependency in web vs. mobile

--- src/store/index.js
+++ src/store/index.js
@@ -1,4 +1,3 @@
-import Vue from 'vue';
 import Vuex from 'vuex';
 import createLogger from 'vuex/dist/logger';
 import matchesModule from './modules/matches';
@@ -7,19 +6,23 @@ import blindsModule from './modules/blinds';
 import sidebarModule from './modules/sidebar';
 import configModule from './modules/config';
 
-Vue.use(Vuex);
-
 const debug = process.env.NODE_ENV !== 'production';
 
-export default new Vuex.Store({
-    modules: {
-        matchesModule, userModule, blindsModule, sidebarModule, configModule,
-    },
-    strict: debug,
-    plugins: debug ? [createLogger()] : [],
-    state: {
-        auth: {
-            token: null,
+// we aren't automatically exporting a store object because of vue-native vs vue
+// instead we expose a function allowing the vue instance to be passed
+export default function createStore(VueInstance) {
+    VueInstance.use(Vuex);
+
+    return new Vuex.Store({
+        modules: {
+            matchesModule, userModule, blindsModule, sidebarModule, configModule,
         },
-    },
-});
+        strict: debug,
+        plugins: debug ? [createLogger()] : [],
+        state: {
+            auth: {
+                token: null,
+            },
+        },
+    });
+};
